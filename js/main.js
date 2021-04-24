ad4223094c5260e295260a73941684474cbe7fa8
gpodder770 <gourabpodder@yandex.com>
2021-04-24 08:00:16 +0600

    Product Section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,85 @@
 $(document).ready(function() {
-  // Owl Carousel
-  $('.owl-carousel').owlCarousel({
+  //Hot deals
+  $('.left-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 2000,
+    navSpeed: 1500,
+    dots: false,
+    navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  })
+
+  //New_Latest_products
+  $('.new_latest_product').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 2000,
+    navSpeed: 1500,
+    dots: false,
+    navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 4
+      }
+    }
+  })
+
+  // Featured Products
+  $('.featured_product').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 2000,
+    navSpeed: 1500,
+    dots: false,
+    navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 3
+      }
+    }
+  })
+
+
+  // Sponsors
+  $('.sponsors-slider').owlCarousel({
     loop: true,
     margin: 50,
     nav: false,
     autoplay: true,
     autoplayTimeout: 3000,
-    autoplaySpeed: 1500,
+    autoplaySpeed: 2000,
     responsive: {
       0: {
         items: 1
